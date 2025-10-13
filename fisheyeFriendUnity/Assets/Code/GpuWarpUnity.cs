78952a8265c089012308aabc15881547175ad06b
Supagoat <supagoa@gmail.com>
2025-10-12 20:38:48 -0400

    Quite close to a v0.1

--- fisheyeFriendUnity/Assets/Code/GpuWarpUnity.cs
+++ fisheyeFriendUnity/Assets/Code/GpuWarpUnity.cs
@@ -9,27 +9,33 @@ namespace FisheyeFriend {
     // Example integration in your existing flow (Unity-only):
     public class GpuWarpUnity : MonoBehaviour {
         public ComputeShader tpsWarpCS;
-        public Texture2D sourceTexture;
-        public RenderTexture outputRT;
 
         FisheyeFriend.TPS2D tps;
         GpuWarper warper;
 
         void Start () {
 
-            Debug.Log("Starting GPU VERSION");
+
+
+            // move to save button  File.WriteAllBytes(config.outPath, readback.EncodeToPNG());
+
+        }
+        void OnDestroy () { warper?.Dispose(); }
+
+
+        public Texture2D WarpImage (Bitmap src) {
             // After you've built TPS in C# from your mapping JSON (unchanged from your code)
             //_kernel = _cs.FindKernel("Warp");
 
             WarpConfig config = new WarpConfig();
             config.mapPath = "D:\\tmp\\hemi_pairs_596.json";
-            config.srcPath = "D:\\tmp\\wallFish.jpg";
-            config.outPath = "D:\\tmp\\wallGPUUnityOut.png";
+            // config.srcPath = "D:\\tmp\\wallFish.jpg";
+            // config.outPath = "D:\\tmp\\wallLanc1GPUUnityOut.png";
             Mapping map = Mapping.Read(config.mapPath);
             tps = TPS2D.FitInverseNormalized(map, config.lambda);
 
-            using Bitmap src = new Bitmap(config.srcPath);
-            sourceTexture = new Texture2D(src.Width, src.Height);
+            // using Bitmap src = new Bitmap(config.srcPath);
+            Texture2D sourceTexture = new Texture2D(src.Width, src.Height);
 
             // copy the src image to sourceTexture
 
@@ -42,11 +48,11 @@ namespace FisheyeFriend {
             sourceTexture.wrapMode = TextureWrapMode.Clamp;
             sourceTexture.filterMode = FilterMode.Bilinear;
 
-            if (outputRT == null) {
-                outputRT = new RenderTexture(sourceTexture.width, sourceTexture.height, 0, RenderTextureFormat.ARGB32);
-                outputRT.enableRandomWrite = true;
-                outputRT.Create();
-            }
+
+            RenderTexture outputRT = new RenderTexture(sourceTexture.width, sourceTexture.height, 0, RenderTextureFormat.ARGB32);
+            outputRT.enableRandomWrite = true;
+            outputRT.Create();
+
 
             warper = new GpuWarper(tpsWarpCS);
             bool clampEdge = /* your flag */ true;
@@ -58,94 +64,91 @@ namespace FisheyeFriend {
             RenderTexture.active = outputRT;
             readback.ReadPixels(new Rect(0, 0, outputRT.width, outputRT.height), 0, 0);
             readback.Apply();
-            File.WriteAllBytes(config.outPath, readback.EncodeToPNG());
-            Debug.Log("ALL DONE WITH GPU VERSION");
+            return readback;
         }
 
-        void OnDestroy () { warper?.Dispose(); }
-    }
+        public class GpuWarper : System.IDisposable {
+            ComputeShader _cs;
+            int _kernel;
 
-    public class GpuWarper : System.IDisposable {
-        ComputeShader _cs;
-        int _kernel;
+            ComputeBuffer _txX, _txY, _txW, _tyX, _tyY, _tyW;
+            bool _inited;
 
-        ComputeBuffer _txX, _txY, _txW, _tyX, _tyY, _tyW;
-        bool _inited;
-
-        public GpuWarper (ComputeShader cs) {
-            _cs = cs;
-            _kernel = _cs.FindKernel("Warp");
-        }
-
-        void EnsureBuffers ((float[] x, float[] y, float[] w) tx, (float[] x, float[] y, float[] w) ty) {
-            ReleaseBuffers();
-            _txX = new ComputeBuffer(tx.x.Length, sizeof(float));
-            _txY = new ComputeBuffer(tx.y.Length, sizeof(float));
-            _txW = new ComputeBuffer(tx.w.Length, sizeof(float));
-            _tyX = new ComputeBuffer(ty.x.Length, sizeof(float));
-            _tyY = new ComputeBuffer(ty.y.Length, sizeof(float));
-            _tyW = new ComputeBuffer(ty.w.Length, sizeof(float));
-
-            _txX.SetData(tx.x); _txY.SetData(tx.y); _txW.SetData(tx.w);
-            _tyX.SetData(ty.x); _tyY.SetData(ty.y); _tyW.SetData(ty.w);
-            _inited = true;
-        }
-
-        public void RenderWholeGPU (Texture src, RenderTexture dst, FisheyeFriend.TPS2D tps, bool clampEdge) {
-            int sw = src.width, sh = src.height;
-            int dw = dst.width, dh = dst.height;
-
-            var tx = tps.ExportTxArrays();
-            var ty = tps.ExportTyArrays();
-            if (!_inited) EnsureBuffers(tx, ty);
-
-            // Coeffs
-            var txSpline = (FisheyeFriend.ThinPlateSpline)typeof(FisheyeFriend.TPS2D)
-                .GetField("tx", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                .GetValue(tps);
-            var tySpline = (FisheyeFriend.ThinPlateSpline)typeof(FisheyeFriend.TPS2D)
-                .GetField("ty", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                .GetValue(tps);
-
-            // Set shader resources
-            _cs.SetTexture(_kernel, "_Src", src);
-            _cs.SetTexture(_kernel, "_Dst", dst);
-
-            _cs.SetBuffer(_kernel, "_TxX", _txX);
-            _cs.SetBuffer(_kernel, "_TxY", _txY);
-            _cs.SetBuffer(_kernel, "_TxW", _txW);
-            _cs.SetBuffer(_kernel, "_TyX", _tyX);
-            _cs.SetBuffer(_kernel, "_TyY", _tyY);
-            _cs.SetBuffer(_kernel, "_TyW", _tyW);
-
-            _cs.SetFloat("a0x", (float)txSpline.A0);
-            _cs.SetFloat("axx", (float)txSpline.AX);
-            _cs.SetFloat("ayx", (float)txSpline.AY);
-            _cs.SetFloat("a0y", (float)tySpline.A0);
-            _cs.SetFloat("axy", (float)tySpline.AX);
-            _cs.SetFloat("ayy", (float)tySpline.AY);
-            _cs.SetVector("dims", new Vector4(sw, sh, dw, dh));
-            _cs.SetInt("nCtrl", tx.x.Length);
-            _cs.SetInt("clampEdge", clampEdge ? 1 : 0);
-
-            // Ensure RW format
-            if (!dst.IsCreated()) {
-                dst.enableRandomWrite = true;
-                dst.Create();
+            public GpuWarper (ComputeShader cs) {
+                _cs = cs;
+                _kernel = _cs.FindKernel("WarpLanc");
             }
 
-            // Dispatch (8x8 thread group)
-            int gx = (dw + 7) / 8;
-            int gy = (dh + 7) / 8;
-            _cs.Dispatch(_kernel, gx, gy, 1);
-        }
+            void EnsureBuffers ((float[] x, float[] y, float[] w) tx, (float[] x, float[] y, float[] w) ty) {
+                ReleaseBuffers();
+                _txX = new ComputeBuffer(tx.x.Length, sizeof(float));
+                _txY = new ComputeBuffer(tx.y.Length, sizeof(float));
+                _txW = new ComputeBuffer(tx.w.Length, sizeof(float));
+                _tyX = new ComputeBuffer(ty.x.Length, sizeof(float));
+                _tyY = new ComputeBuffer(ty.y.Length, sizeof(float));
+                _tyW = new ComputeBuffer(ty.w.Length, sizeof(float));
 
-        public void Dispose () => ReleaseBuffers();
-        void ReleaseBuffers () {
-            _txX?.Dispose(); _txY?.Dispose(); _txW?.Dispose();
-            _tyX?.Dispose(); _tyY?.Dispose(); _tyW?.Dispose();
-            _txX = _txY = _txW = _tyX = _tyY = _tyW = null;
-            _inited = false;
+                _txX.SetData(tx.x); _txY.SetData(tx.y); _txW.SetData(tx.w);
+                _tyX.SetData(ty.x); _tyY.SetData(ty.y); _tyW.SetData(ty.w);
+                _inited = true;
+            }
+
+            public void RenderWholeGPU (Texture src, RenderTexture dst, FisheyeFriend.TPS2D tps, bool clampEdge) {
+                int sw = src.width, sh = src.height;
+                int dw = dst.width, dh = dst.height;
+
+                var tx = tps.ExportTxArrays();
+                var ty = tps.ExportTyArrays();
+                if (!_inited) EnsureBuffers(tx, ty);
+
+                // Coeffs
+                var txSpline = (FisheyeFriend.ThinPlateSpline)typeof(FisheyeFriend.TPS2D)
+                    .GetField("tx", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                    .GetValue(tps);
+                var tySpline = (FisheyeFriend.ThinPlateSpline)typeof(FisheyeFriend.TPS2D)
+                    .GetField("ty", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                    .GetValue(tps);
+
+                // Set shader resources
+                _cs.SetTexture(_kernel, "_Src", src);
+                _cs.SetTexture(_kernel, "_Dst", dst);
+
+                _cs.SetBuffer(_kernel, "_TxX", _txX);
+                _cs.SetBuffer(_kernel, "_TxY", _txY);
+                _cs.SetBuffer(_kernel, "_TxW", _txW);
+                _cs.SetBuffer(_kernel, "_TyX", _tyX);
+                _cs.SetBuffer(_kernel, "_TyY", _tyY);
+                _cs.SetBuffer(_kernel, "_TyW", _tyW);
+
+                _cs.SetFloat("a0x", (float)txSpline.A0);
+                _cs.SetFloat("axx", (float)txSpline.AX);
+                _cs.SetFloat("ayx", (float)txSpline.AY);
+                _cs.SetFloat("a0y", (float)tySpline.A0);
+                _cs.SetFloat("axy", (float)tySpline.AX);
+                _cs.SetFloat("ayy", (float)tySpline.AY);
+                _cs.SetVector("dims", new Vector4(sw, sh, dw, dh));
+                _cs.SetInt("nCtrl", tx.x.Length);
+                _cs.SetInt("clampEdge", clampEdge ? 1 : 0);
+
+                // Ensure RW format
+                if (!dst.IsCreated()) {
+                    dst.enableRandomWrite = true;
+                    dst.Create();
+                }
+
+                // Dispatch (8x8 thread group)
+                int gx = (dw + 7) / 8;
+                int gy = (dh + 7) / 8;
+                _cs.Dispatch(_kernel, gx, gy, 1);
+            }
+
+            public void Dispose () => ReleaseBuffers();
+            void ReleaseBuffers () {
+                _txX?.Dispose(); _txY?.Dispose(); _txW?.Dispose();
+                _tyX?.Dispose(); _tyY?.Dispose(); _tyW?.Dispose();
+                _txX = _txY = _txW = _tyX = _tyY = _tyW = null;
+                _inited = false;
+            }
         }
     }
 }
--- fisheyeFriendUnity/Assets/Code/IO.cs
+++ fisheyeFriendUnity/Assets/Code/IO.cs
@@ -2,6 +2,8 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Drawing;
+using System.Drawing.Imaging;
 using SFB;
 
 namespace FisheyeFriend {
@@ -15,10 +17,10 @@ namespace FisheyeFriend {
         public Button saveImageButton; // "Save" button
 
         // Assign in the Inspector: RawImage for displaying the resized image (left)
-        public RawImage leftImage; // Left RawImage
+        public RawImage originalImage; // Left RawImage
 
         // Assign in the Inspector: RawImage for displaying the resized image (right)
-        public RawImage rightImage; // Right RawImage
+        public RawImage processedImage; // Right RawImage
 
         // Assign in the Inspector: ScrollRect containing both images
         public ScrollRect scrollRect; // ScrollRect for side-by-side images
@@ -29,10 +31,64 @@ namespace FisheyeFriend {
         // Store the resized image
         private Texture2D resizedTexture;
 
+
+
+        public GpuWarpUnity gpuWarp;
+
+
         void Start () {
             openImageButton.onClick.AddListener(OnOpenImageClicked);
             saveImageButton.onClick.AddListener(OnSaveImageClicked);
             saveImageButton.interactable = false; // Disable until an image is loaded
+
+            Vector3 leftEdge = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, openImageButton.transform.position.z));
+            Vector3 topEdge = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, openImageButton.transform.position.z));
+
+           // openImageButton.transform.position = new Vector3(leftEdge.x + openImageButton.GetComponent<RectTransform>().rect.width / 2, topEdge.y - openImageButton.GetComponent<RectTransform>().rect.height / 2, openImageButton.transform.position.z);
+        }
+
+        void Update () {
+           if(Input.GetKeyDown(KeyCode.Tab)) {
+                SwapImages();
+            }
+           if (Input.GetKeyDown(KeyCode.Escape)) {
+                Application.Quit();
+            }
+
+           if(Input.GetKey(KeyCode.W)) {
+                originalImage.transform.position -= new Vector3(0, 1* originalImage.transform.localScale.y, 0);
+                processedImage.transform.position -= new Vector3(0, 1 * processedImage.transform.localScale.y, 0);
+            }
+            if (Input.GetKey(KeyCode.S)) {
+                originalImage.transform.position += new Vector3(0, 1 * originalImage.transform.localScale.y, 0);
+                processedImage.transform.position += new Vector3(0, 1 * processedImage.transform.localScale.y, 0);
+            }
+
+            if (Input.GetKey(KeyCode.A)) {
+                originalImage.transform.position += new Vector3(1 * originalImage.transform.localScale.x, 0, 0);
+                processedImage.transform.position += new Vector3(1 * processedImage.transform.localScale.x, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.D)) {
+                originalImage.transform.position -= new Vector3(1 * originalImage.transform.localScale.x, 0, 0);
+                processedImage.transform.position -= new Vector3(1 * processedImage.transform.localScale.x, 0, 0);
+            }
+
+            if (Input.mouseScrollDelta.y != 0f) {
+                originalImage.transform.localScale += new Vector3(Input.mouseScrollDelta.y, Input.mouseScrollDelta.y, 0);
+                processedImage.transform.localScale += new Vector3(Input.mouseScrollDelta.y, Input.mouseScrollDelta.y, 0);
+            }
+        }
+
+        void SwapImages () {
+            if(!originalImage.gameObject.activeSelf && !processedImage.gameObject.activeSelf) {
+                processedImage.gameObject.SetActive(true);
+            } else if (originalImage.gameObject.activeSelf) {
+                originalImage.gameObject.SetActive(false);
+                processedImage.gameObject.SetActive(true);
+            } else {
+                originalImage.gameObject.SetActive(true);
+                processedImage.gameObject.SetActive(false);
+            }
         }
 
         void OnOpenImageClicked () {
@@ -42,10 +98,64 @@ namespace FisheyeFriend {
             string path = ShowFilePicker(); // Implement this with a plugin or native dialog
 
             if (!string.IsNullOrEmpty(path) && File.Exists(path)) {
-                byte[] fileData = File.ReadAllBytes(path);
-                originalTexture = new Texture2D(2, 2);
-                if (originalTexture.LoadImage(fileData)) {
-                    resizedTexture = ResizeTextureMaintainingAspect(originalTexture, 900, 400);
+                //byte[] fileData = File.ReadAllBytes(path);
+                //  originalTexture = new Texture2D(2, 2);
+
+                Bitmap source = new Bitmap(path); // your loaded Bitmap
+                Texture2D tex = BitmapToTexture2D(source);
+                originalImage.texture = tex;
+
+                //  if (originalTexture.LoadImage(fileData)) {
+
+                //   byte[] sourceBytes = ((Texture2D)originalTexture.texture).EncodeToPNG();
+
+                Texture2D wrarped = gpuWarp.WarpImage(source);
+                   /* Bitmap warpedBM;
+                    using (var warpMS = new MemoryStream(wrarped.EncodeToPNG())) {
+                        warpedBM = new Bitmap(warpMS);
+                    }
+                    warpedBM.Save("D:\\tmp\\wallUnityGPULanc.png", ImageFormat.Png);
+                   */
+                    processedImage.texture = wrarped;
+
+                // Enable the save button
+                saveImageButton.interactable = true;
+                SwapImages();
+             //   }
+            }
+        }
+
+        void OnSaveImageClicked () {
+            TextureToBitmap((Texture2D)processedImage.texture).Save("D:\\tmp\\wallUnityGPULancSave.png", ImageFormat.Png);
+        }
+
+        public Bitmap TextureToBitmap(Texture2D texture) {
+            // Stashing an alternate way to convert
+            /* byte[] bytes = texture.EncodeToPNG();
+             using (var ms = new MemoryStream(bytes)) {
+                 return new Bitmap(ms);
+             }*/
+
+            Bitmap bm;
+            using (var warpMS = new MemoryStream(texture.EncodeToPNG())) {
+                bm = new Bitmap(warpMS);
+            }
+            return bm;
+        }
+
+
+        public static Texture2D BitmapToTexture2D (Bitmap bitmap) {
+            using (var memory = new MemoryStream()) {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+                memory.Position = 0;
+                byte[] bytes = memory.ToArray();
+                Texture2D tex = new Texture2D(2, 2);
+                tex.LoadImage(bytes);
+                return tex;
+            }
+        }
+        /**
+         resizedTexture = ResizeTextureMaintainingAspect(originalTexture, 1920/2, 1080);
 
                     // Display the resized image on both sides
                     leftImage.texture = resizedTexture;
@@ -56,16 +166,12 @@ namespace FisheyeFriend {
                     leftImage.rectTransform.sizeDelta = size;
                     rightImage.rectTransform.sizeDelta = size;
 
-                    // Enable the save button
-                    saveImageButton.interactable = true;
-                }
-            }
-        }
+         Bitmap rightBM;
+                    using (var ms = new MemoryStream(pngData)) {
+                        rightBM = new Bitmap(ms);
+                    }
+        */
 
-        void OnSaveImageClicked () {
-            // Placeholder for save logic
-            SaveOriginalImage();
-        }
 
         // Resize the texture maintaining aspect ratio
         Texture2D ResizeTextureMaintainingAspect (Texture2D source, int maxWidth, int maxHeight) {
@@ -79,7 +185,7 @@ namespace FisheyeFriend {
                 for (int x = 0; x < newWidth; x++) {
                     float u = x / (float)(newWidth - 1);
                     float v = y / (float)(newHeight - 1);
-                    Color color = source.GetPixelBilinear(u, v);
+                    UnityEngine.Color color = source.GetPixelBilinear(u, v);
                     result.SetPixel(x, y, color);
                 }
             }
@@ -87,17 +193,14 @@ namespace FisheyeFriend {
             return result;
         }
 
-        // Placeholder for file picker (implement with a plugin or native dialog)
+
         string ShowFilePicker () {
             var paths = StandaloneFileBrowser.OpenFilePanel("Open File", "", "", false);
             return paths[0];
         }
 
-        // Placeholder for save logic
-        void SaveOriginalImage () {
-            // TODO: Implement save dialog and write originalTexture to file
-            Debug.Log("SaveOriginalImage called.");
-        }
+
+
 
 
     }
